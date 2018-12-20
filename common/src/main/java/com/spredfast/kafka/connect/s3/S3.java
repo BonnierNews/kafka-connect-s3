4699fded57d43ed5f737dbd25263e237b9da636b
Magnus Spångdal <magnus@spangdal.se>
2018-12-20 19:36:32 +0100

    Make it work. Upgrade sdk to 1.11.472
    Default signer to the legacy S3Signer
    
    allows overriding signertype. set to empty string to not set a signer override.

--- common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
+++ common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
@@ -1,28 +1,36 @@
 package com.spredfast.kafka.connect.s3;
 
-import java.util.Map;
-import java.util.Objects;
-
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.util.Map;
 
 public class S3 {
 
 	public static AmazonS3 s3client(Map<String, String> config) {
-		// Use default credentials provider that looks in Env + Java properties + profile + instance role
-		AmazonS3 s3Client = new AmazonS3Client();
+		AmazonS3ClientBuilder s3ClientBuilder = AmazonS3ClientBuilder.standard();
+		ClientConfiguration clientConfig = new ClientConfiguration();
 
-		// If worker config sets explicit endpoint override (e.g. for testing) use that
-		String s3Endpoint = config.get("s3.endpoint");
-		if (s3Endpoint != null && !Objects.equals(s3Endpoint, "")) {
-			s3Client.setEndpoint(s3Endpoint);
+		Boolean useS3PathStyle = Boolean.parseBoolean(config.get("s3.path_style"));
+		s3ClientBuilder.withPathStyleAccessEnabled(useS3PathStyle);
+
+		String endpoint = config.get("s3.endpoint");
+		if (endpoint != null && endpoint.trim().length() > 0) {
+			s3ClientBuilder.withEndpointConfiguration(
+				new EndpointConfiguration(endpoint, Regions.EU_WEST_1.getName())
+			);
 		}
-		Boolean s3PathStyle = Boolean.parseBoolean(config.get("s3.path_style"));
-		if (s3PathStyle) {
-			s3Client.setS3ClientOptions(S3ClientOptions.builder().setPathStyleAccess(true).build());
+
+		String signer = config.getOrDefault("s3.signer", "S3SignerType");
+		if (signer != null && signer.trim().length() > 0) {
+			clientConfig.setSignerOverride(signer);
+
 		}
-		return s3Client;
+
+		return s3ClientBuilder.withClientConfiguration(clientConfig).build();
 	}
 
 }
